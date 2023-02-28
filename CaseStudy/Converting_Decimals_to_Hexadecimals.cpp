f28a3e6e9d5114ce0388ecc34e3a80afcfa07774
satlong00 <nguyencongquang14022004@gmail.com>
2023-02-28 18:40:59 +0700

    Create Converting_Decimals_to_Hexadecimals.cpp

---
+++ CaseStudy/Converting_Decimals_to_Hexadecimals.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout << "Enter a decimal number: ";
+    int decimal;
+    cin >> decimal;
+    string hex = "";
+    while(decimal > 0)
+    {
+        
+    }
+}
