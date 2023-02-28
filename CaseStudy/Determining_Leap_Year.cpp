91a8373ff6a3305be1cabc7ee669f0abb11c0c96
satlong00 <ncq.kmp@github.com>
2023-02-28 14:27:20 +0700

    test

--- CaseStudy/Determining_Leap_Year.cpp
+++ CaseStudy/Determining_Leap_Year.cpp
@@ -1,1 +1,14 @@
+//requirement: A year is a leap year if it is divisible by 4 but not by 100, or if it is divisible by 400
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    int year;
+    cout << "Nhap vao nam ma ban muon kiem tra: " << endl;
+    cin >> year ;
+    if(year % 400 == 0)
+        cout << year << " la nam nhuan" << endl;
+    else
+        cout << year << " khong phai la nam nhuan" << endl;
+}
